8198fe8bfeb4ce2183f558aaf252577353ef0c94
Artem Kandakov <Temofey1993@gmail.com>
2022-08-29 19:21:29 +0500

    Задание №41

---
+++ Exercise_41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i<arr.Length; i++)
+    {
+        Console.Write(arr[i]+" ");
+    }
+}
+Console.WriteLine("Введите, сколько чисел Вы планируете ввести");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+int result = 0;
+Console.WriteLine("Введите Ваши числа");
+for(int i = 0; i<m; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if(array[i]>0)
+    {
+        result++;
+    }
+}
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(result);
